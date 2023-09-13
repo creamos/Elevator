84bbcf21718dd0dbb7d210bcf6165550138eaac2
J-C <daxxas158@gmail.com>
2023-09-13 11:20:14 +0200

    added score ui

---
+++ Assets/Scripts/Score/ScoreUI.cs
@@ -0,0 +1,29 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Score
+{
+    public class ScoreUI : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI scoreTMP;
+        
+        private int score = 0;
+        
+        public void AddScore(int amount)
+        {
+            score += amount;
+            scoreTMP.text = score.ToString();
+        }
+        
+        public void ResetScore()
+        {
+            score = 0;
+            scoreTMP.text = score.ToString();
+        }
+
+        public int GetScore()
+        {
+            return score;
+        }
+    }
+}
