6d9993289dfe81c0fa4d1bcb6018eae673ed2415
Alex <alexandrelacroix.35@gmail.com>
2023-09-13 10:25:40 +0200

    add scriptable events package, add gameover detection, add floor number indication

---
+++ Assets/Scripts/Debug/SetFloorTextID.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetFloorTextID : MonoBehaviour
+{
+    [SerializeField] private Floor floor;
+    [SerializeField] private Text text;
+
+    private void OnEnable()
+    {
+        floor.Initialized -= SetText;
+        floor.Initialized += SetText;
+    }
+
+    private void OnDisable()
+    {
+        floor.Initialized -= SetText;
+    }
+
+    private void SetText()
+    {
+        text.text = floor.DisplayIndex.ToString();
+    }
+}
--- Assets/Scripts/GameLoop/GameManager.cs
+++ Assets/Scripts/GameLoop/GameManager.cs
@@ -1,14 +1,40 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+using NaughtyAttributes;
+using ScriptableEvents;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private SpawnManager spawnManager;
 
+    [field: SerializeField, BoxGroup("Raised Events")]
+    public GameEvent GameStart  { get; private set; }
+    
+    [field: SerializeField, BoxGroup("Raised Events")]
+    public GameEvent GameOver  { get; private set; }
+    
+    [field: SerializeField, BoxGroup("Raised Events")]
+    public BoolEvent GamePause { get; private set; }
+
+    [SerializeField, BoxGroup("Listened Events")]
+    private GameEvent queueOverflow;
+
+    private void OnEnable()
+    {
+        if (queueOverflow)
+        {
+            queueOverflow.OnTriggered -= OnGameOver;
+            queueOverflow.OnTriggered += OnGameOver;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (queueOverflow) queueOverflow.OnTriggered -= OnGameOver;
+    }
+
     private void Start()
     {
+        GameStart.Raise();
         spawnManager.StartSpawnLoop();
     }
 
@@ -16,8 +42,12 @@ public class GameManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Tab))
         {
-            if(spawnManager.IsRunning) spawnManager.StopSpawnLoop();
-            else spawnManager.StartSpawnLoop();
+            GameOver.Raise();
         }
+    }
+
+    private void OnGameOver()
+    {
+        GameOver.Raise();
     }
 }
