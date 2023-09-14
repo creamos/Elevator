b2aa7175bbc2eed5ce5d4e9b4d9214c2ab2cc403
J-C <daxxas158@gmail.com>
2023-09-14 16:23:31 +0200

    sound events

---
+++ Assets/Scripts/Audio/SoundEvent.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Audio
+{
+    [CreateAssetMenu(fileName = "FILENAME", menuName = "MENUNAME", order = 0)]
+    public class SoundEvent : ScriptableObject
+    {
+        
+    }
+}
